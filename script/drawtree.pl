1d5114965a048c0bd12e8e470b2b61afb7207ca0
Rutger Vos <rutgeraldo@gmail.com>
2012-03-05 21:42:47 +0100

    Added intelligence for setting image height based on ntax

--- script/drawtree.pl
+++ script/drawtree.pl
@@ -6,6 +6,12 @@ use Bio::Phylo::IO 'parse';
 use Bio::Phylo::Treedrawer;
 use Bio::Phylo::Util::CONSTANT ':objecttypes';
 use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::Factory;
+
+my $fac = Bio::Phylo::Factory->new(
+    'node' => 'Bio::Phylo::Forest::DrawNode',
+    'tree' => 'Bio::Phylo::Forest::DrawTree',
+);
 
 # processing command line arguments
 my ( $infile, $format, $outfile, %defines, $verbose );
@@ -23,14 +29,19 @@ my $logger = Bio::Phylo::Util::Logger->new( '-level' => $verbose );
 # parse first tree from input file
 $logger->info("Going to read $format tree from $infile");
 my ($tree) = @{ parse(
-    '-format' => $format,
-    '-file'   => $infile,
+    '-format'     => $format,
+    '-file'       => $infile,
     '-as_project' => 1,
+    '-factory'    => $fac,
 )->get_items(_TREE_) };
 
 # instantiate tree drawer
 $logger->info("Going to draw using these settings: " . Dumper(\%defines));
-my $td = Bio::Phylo::Treedrawer->new(%defines);
+my $td = Bio::Phylo::Treedrawer->new(
+    '-tree'   => $tree,
+    '-height' => $tree->get_ntax * 50,
+    %defines,
+);
 
 # open output handle
 open my $fh, '>', $outfile or die "Can't open $outfile: $!";
