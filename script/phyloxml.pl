c2f49c947c1c4b76dd5104f7aad57fa646d0b0f1
Rutger Vos <rutgeraldo@gmail.com>
2012-03-06 15:52:30 +0100

    script now correctly re-uses previously existing annotations, and writes compact nexml

--- script/phyloxml.pl
+++ script/phyloxml.pl
@@ -35,24 +35,31 @@ my $project = parse(
     '-as_project' => 1
 );
 
+sub update_meta {
+    my ( $object, $predicate, $value, %ns ) = @_;
+    my ( $meta ) = @{ $object->get_meta($predicate) };
+    if ( $meta ) {
+        $meta->set_triple( $predicate => $value );
+    }
+    else {
+        $object->add_meta(
+            $fac->create_meta(
+                '-namespaces' => \%ns,
+                '-triple' => { $predicate => $value },
+            )
+        );
+    }
+}
+
 for my $taxon ( @{ $project->get_items(_TAXON_) } ) {
     my $name = $taxon->get_name;
     if ( $name =~ m/^([^_]+_[^_]+_[^_]+)/ ) {
         my $key = $1;
         if ( $map{$key} ) {
             my $code = join '', map { substr $_, 0, 5 } map { uc $_ } split / /, $map{$key}->[0];
-            $taxon->add_meta(
-                $fac->create_meta(
-                    '-namespaces' => { 'pxml' => _NS_PHYLOXML_ },
-                    '-triple' => { 'pxml:code' => $code },
-                )
-            );
-            $taxon->add_meta(
-                $fac->create_meta(
-                    '-namespaces' => { 'pxml' => _NS_PHYLOXML_ },
-                    '-triple' => { 'pxml:scientific_name' => $map{$key}->[0] },
-                )
-            )            
+            my %ns = ( 'pxml' => _NS_PHYLOXML_ );
+            update_meta( $taxon, 'pxml:code' => $code, %ns );
+            update_meta( $taxon, 'pxml:scientific_name' => $map{$key}->[0], %ns );
         }
         else {
             warn 'wtf:', $key;
@@ -64,4 +71,9 @@ for my $taxon ( @{ $project->get_items(_TAXON_) } ) {
 
 }
 
+{
+    open my $fh, '>', $infile or die $!;
+    print $fh $project->to_xml( '-compact' => 1 );
+    close $fh;
+}
 print unparse( '-format' => 'phyloxml', '-phylo' => $project );
