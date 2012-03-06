0c0c21f8c866bcea666ebb5ad630f03505e446ae
Rutger Vos <rutgeraldo@gmail.com>
2012-03-06 23:17:12 +0100

    More reporting back to user, also no longer downloads same file multiple times.

--- script/fetch_trees.pl
+++ script/fetch_trees.pl
@@ -6,9 +6,15 @@ use Bio::Phylo::PhyloWS::Client;
 use Bio::Phylo::Factory;
 use Bio::Phylo::IO 'parse';
 
+my $default_dir = $0;
+$default_dir =~ s|fetch_trees\.pl$|../data/sourcetrees/|;
+
+my $default_csv = $0;
+$default_csv =~ s|fetch_trees\.pl$|../data/excel/taxa.csv|;
+
 # read csv file
-my $file = shift @ARGV;
-my $dir = shift @ARGV || 'result';
+my $file = shift @ARGV || $default_csv;
+my $dir = shift @ARGV || $default_dir;
 my $map = Bio::Phylo::Cobra::TaxaMap->new($file);
 my @ids = $map->get_distinct_taxonIDs;
 
@@ -20,6 +26,8 @@ my $client = $fac->create_client(
 );
 
 # fetch all trees for all identifiers
+my %seen;
+my @notseen;
 for my $id ( @ids ) {
 	eval {
 		my $desc = $client->get_query_result(
@@ -30,24 +38,61 @@ for my $id ( @ids ) {
 		
 		# XXX there is something wrong or weird about the guid from treebase
 		for my $res ( @{ $desc->get_entities } ) {
-			my $url = $res->get_guid . '?format=nexus';
-			my $proj = parse( 
-				'-format'     => 'nexus', 
-				'-url'        => $res->get_guid . '?format=nexus',
-				'-as_project' => 1,
-			);
+			my $guid = $res->get_guid;
 			
-			# write the result to a file based on the GUID.
-			# this should clobber instance where multiple taxa in our list
-			# were in the same study. that's the behavior that we want.
-			my $outfile = $res->get_guid;
-			$outfile =~ s|^.+:(.+)$|$dir/$1.nex|;			
-			open my $fh, '>', $outfile or die $!;
-			print $fh $proj->to_nexus, "\n";
-			warn "taxon $id written to $outfile";
+			# don't download twice
+			if ( not $seen{$guid} ) {
+				
+				# fetch data
+				my $url  = $guid . '?format=nexml';
+				my $proj = parse( 
+					'-format'     => 'nexml', 
+					'-url'        => $url,
+					'-as_project' => 1,
+				);
+				
+				# write the result to a file based on the GUID.
+				# this should clobber instance where multiple taxa in our list
+				# were in the same study. that's the behavior that we want.
+				my $outfile = $guid;
+				$outfile =~ s|^.+:(.+)$|$dir/$1.xml|;			
+				open my $fh, '>', $outfile or die "Can't open $outfile: $!";
+				print $fh $proj->to_xml, "\n";
+				warn "taxon $id written to $outfile";
+			}
+			
+			# counting seen files so we can count overlap
+			$seen{$guid}++;
 		}
+		
+		# zero hits
+		push @notseen, $id unless scalar( @{ $desc->get_entities } );
 	};
 	if ( $@ ) {
 		warn "problem with taxon $id: $@";
 	}
 }
+
+# reporting back
+my $ntax = scalar(@ids);
+my $filecount = scalar(keys(%seen));
+my $average = $ntax / $filecount;
+print "$average overlapping taxa in $filecount files\n";
+print "taxa with 0 hits:\n";
+print join "\n", @notseen;
+
+__DATA__
+0.391691394658754 overlapping taxa in 337 files
+taxa with 0 hits:
+111304
+186611
+196418
+310520
+33626
+338837
+338838
+51750
+61970
+672774
+865857
+8781
