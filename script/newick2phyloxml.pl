845637b94a048521d3b437494efc515f6d23f9f2
Rutger Vos <rutgeraldo@gmail.com>
2012-03-06 17:13:34 +0100

    Now uses taxa.csv to get species codes, instead of re-computing them. Also added comments.

--- script/newick2phyloxml.pl
+++ script/newick2phyloxml.pl
@@ -4,29 +4,69 @@ use warnings;
 use Bio::Phylo::Factory;
 use Bio::Phylo::IO qw'parse unparse';
 use Bio::Phylo::Util::CONSTANT qw':objecttypes :namespaces';
+use Getopt::Long;
 
+# get command line arguments
+my ( $infile, $taxamap, %map );
+GetOptions(
+    'infile=s'  => \$infile,
+    'taxamap=s' => \$taxamap,
+);
+
+# parse input tree
 my $project = parse(
     '-format'     => 'newick',
-    '-file'       => shift(@ARGV),
+    '-file'       => $infile,
     '-as_project' => 1,
 );
 
+# parse input table
+{
+    open my $fh, '<', $taxamap or die $!;
+    while(<$fh>) {
+        chomp;
+        my @fields = split /,/, $_;
+        shift @fields;
+        my $key    = shift @fields;
+        $map{$key} = \@fields;
+    }
+}
+
+# create taxa for the forest object
 my ($forest) = @{ $project->get_items(_FOREST_) };
 my $taxa     = $forest->make_taxa;
+
+# instantiate factory to make annotation objects
 my $factory  = Bio::Phylo::Factory->new;
 
+# iterate over newly created taxa
 for my $taxon ( @{ $taxa->get_entities } ) {
     my $name = $taxon->get_name;
+    
+    # clean up name in case it has quotes and/or underscores
     $name =~ s/['"]//g;
     $name =~ s/_/ /g;
     $_->set_name($name) for @{ $taxon->get_nodes };
-    my $code = join '', map { substr $_, 0, 5 } map { uc $_ } split / /, $name;
+    
+    # code is obtained form taxa.csv
+    my $code = $map{$name}->[1];
+    
+    # attach code as annotation
     $taxon->add_meta(
         $factory->create_meta(
             '-namespaces' => { 'pxml'  => _NS_PHYLOXML_ },
             '-triple' => { 'pxml:code' => $code },
         )
     );
+    
+    # attach scientific_name as annotation
+    $taxon->add_meta(
+        $factory->create_meta(
+            '-namespaces' => { 'pxml'  => _NS_PHYLOXML_ },
+            '-triple' => { 'pxml:scientific_name' => $name },
+        )
+    );    
 }
 
+# write output
 print unparse( '-format' => 'phyloxml', '-phylo' => $project );
