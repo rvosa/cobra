926942379208cd29b047502492eb2c46f0300b01
Rutger Vos <rutgeraldo@gmail.com>
2012-03-07 20:19:58 +0100

    added more comments

--- script/make_consensus.pl
+++ script/make_consensus.pl
@@ -7,12 +7,14 @@ use Bio::Phylo::IO qw'parse unparse';
 use Bio::Phylo::Util::CONSTANT qw':objecttypes :namespaces';
 use Bio::Phylo::Factory;
 
+# process command line arguments
 my ( $infile, $csv );
 GetOptions(
     'infile=s' => \$infile,
     'csv=s'    => \$csv,
 );
 
+# build nexus string from $infile only keeping distinct trees
 my %seen;
 my $nexus_string;
 {
@@ -28,15 +30,21 @@ my $nexus_string;
     }
 }
 
+# parse nexus string with distinct trees
 my $project = parse(
     '-format' => 'nexus',
     '-string' => $nexus_string,
     '-as_project' => 1
 );
 
+# build consensus tree
 my ($forest) = @{ $project->get_items(_FOREST_) };
 my $tree = $forest->make_consensus;
+
+# delete original forest with distinct trees
 $project->delete($forest);
+
+# create new forest, add consensus tree, make taxa
 my $fac = Bio::Phylo::Factory->new;
 $forest = $fac->create_forest;
 $forest->insert($tree);
@@ -44,9 +52,11 @@ my $taxa = $forest->make_taxa;
 $forest->set_taxa($taxa);
 $project->insert($taxa,$forest);
 
+# re-root on MRP outgroup
 my $outgroup = $tree->get_by_name("'mrp_outgroup'");
 $outgroup->set_root_below;
 
+# warn for polytomies
 $tree->visit(
     sub {
         my $node = shift;
@@ -55,8 +65,12 @@ $tree->visit(
         }
     }
 );
+
+# resolve polytomies
 $tree->resolve;
 
+# attach 10-character code (from csv) and binomial
+# as phyloxml annotations
 my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
 $taxa->visit(
     sub {
@@ -82,4 +96,5 @@ $taxa->visit(
     }
 );
 
+# print output
 print unparse( '-format' => 'phyloxml', '-phylo' => $project );
