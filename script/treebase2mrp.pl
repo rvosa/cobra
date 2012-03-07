754bdafc8acbb27cfe43ef2cab39458a45a10bce
Rutger Vos <rutgeraldo@gmail.com>
2012-03-07 20:34:04 +0100

    added more comments

--- script/treebase2mrp.pl
+++ script/treebase2mrp.pl
@@ -7,6 +7,7 @@ use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::Util::CONSTANT ':objecttypes';
 use Data::Dumper;
 
+# process command line arguments
 my ( $infile, $format, $csv );
 GetOptions(
     'infile=s' => \$infile,
@@ -14,9 +15,11 @@ GetOptions(
     'csv=s'    => \$csv,
 );
 
+# create seen hash for NCBI taxon ids
 my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
 my %seen = map { $_ => 1 } $map->get_all_taxonIDs;
 
+# parse tree block from input file
 my ($forest) = @{
     parse(
         '-format' => $format,
@@ -25,6 +28,9 @@ my ($forest) = @{
     )->get_items(_FOREST_)
 };
 
+# for each tip in each tree, fetch its taxon object and from that get the
+# skos:*match annotations, which may have an NCBI taxon id. if it does,
+# copy it over to the tip and taxon name
 for my $tree ( @{ $forest->get_entities } ) {
     for my $tip ( @{ $tree->get_terminals } ) {
         my $taxon = $tip->get_taxon;
@@ -40,7 +46,11 @@ for my $tree ( @{ $forest->get_entities } ) {
     }
 }
 
+# create mrp matrix
 my $matrix = $forest->make_matrix;
+
+# create a simple hash keyed on ncbi taxon ids, with values the character
+# state sequences. only keep those key value pairs that are seen in taxa.csv
 my %simple;
 my $nchar;
 for my $row ( @{ $matrix->get_entities } ) {
@@ -52,6 +62,9 @@ for my $row ( @{ $matrix->get_entities } ) {
     }
 }
 
+# only keep phylogenetically informative columns. these mrp matrices *can*
+# have uninformative columns because we've pruned rows. also, they now may
+# have duplicate 'site patterns', which we also prune
 my %informative = map { $_ => [] } keys %simple;
 my @names = keys %simple;
 my %pattern;
@@ -69,6 +82,8 @@ for my $i ( 0 .. ( $nchar - 1 ) ) {
     }
     $pattern{$pattern}++;
 }
+
+# print as simple key/value table
 for my $row ( keys %informative ) {
     print $row, "\t", join('', @{ $informative{$row} } ), "\n";
 }
