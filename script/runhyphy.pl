b9aca93776f5705b2553c11f7d60f08b64dbe93c
Rutger Vos <rutgeraldo@gmail.com>
2012-03-12 18:09:53 +0100

    this script runs hyphy with the commands generated by hyphywrapper.pl

---
+++ script/runhyphy.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+my ( $hyphydir, $executable, $infile ) = ( $ENV{'HYPHY_DIR'}, 'HYPHYMP' );
+GetOptions(
+	'hyphydir=s'   => \$hyphydir,
+	'executable=s' => \$executable,
+	'infile=s'     => \$infile,
+);
+unless( $hyphydir && -d $hyphydir && $executable && -x "${hyphydir}/${executable}" && $infile && -f $infile ) {
+	die "Usage: $0 --infile=<infile> [--hyphydir=<hyphydir>] [--executable=<hyphy>]";
+}
+
+chdir $hyphydir;
+system( "./$executable < $infile" ) == 0 or warn "problem executing ${infile}: $?\n";
