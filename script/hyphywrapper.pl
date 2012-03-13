9123c588ddfad70c9e98ced6af52aa2063489761
Rutger Vos <rutgeraldo@gmail.com>
2012-03-13 20:15:45 +0100

    Now writes commands for BranchSiteREL

--- script/hyphywrapper.pl
+++ script/hyphywrapper.pl
@@ -26,12 +26,14 @@ my ( $forest ) = @{ parse(
 
 my $tree = $forest->first;
 my $nodes = scalar( @{ $tree->get_entities } ) - 2;
+my $outfile = $infile;
+$outfile =~ s/.[^\.]+$/.hyphyout/;
 
 # (10) Positive Selection
-print "10\n";
+#print "10\n";
 
 # (3) Test whether a branch (or branches) in the tree evolves under different dN and dS than the rest of the tree.
-print "3\n";
+#print "3\n";
 
 # Choose Genetic Code
 # (1):[Universal] Universal code. (Genebank transl_table=1).
@@ -42,25 +44,27 @@ print "$infile\n\n";
 
 # Site-to-site rate variation model
 # (1):[None] No site-by-site rate variation.
-print "1\n";
+#print "1\n";
 
 # Nucleotide Substitution Model
 # (1):[Default] MG94xHKY85. Only corrects for transition/transversion bias.
-print "1\n";
+#print "1\n";
 
 # Amino Acid Class Model
 # (1):[Default] Single non-synonymous rate class. All changes are equivalent
-print "1\n";
+#print "1\n";
 
 # A tree was found in the data file:
 # Would you like to use it:
 print "Y\n";
 
 # Choose the branch to test:
-print "$_\n" for 1 .. $nodes;
-print "d\n"; # done
+#print "$_\n" for 1 .. $nodes;
+#print "d\n"; # done
 
 # Parameters to test:
 # (1):[1] Non-synonymous rate parameter nsClass1
-print "1\n";
-print "d\n"; # done
+#print "1\n";
+#print "d\n"; # done
+
+print "$outfile\n\n";
