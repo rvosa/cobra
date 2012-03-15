53660bb0e9da771cab115591d71abc7d1d78b57d
Rutger Vos <rutgeraldo@gmail.com>
2012-03-15 19:22:11 +0100

    The NCBI common tree is now written out as a constraint tree with CODES as tip names. Because the NCBI common tree had some subspecies that map onto the same CODE we now need to prune these out. In actuality this is only the case for CROTDURI.

--- script/make_ratchet_footer.pl
+++ script/make_ratchet_footer.pl
@@ -3,16 +3,21 @@ use warnings;
 use strict;
 use Getopt::Long;
 use Bio::Phylo::IO 'parse';
+use Bio::Phylo::Cobra::TaxaMap;
 use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # process command line arguments
-my ( $outgroup, $constr, $format, $ratchet );
+my ( $outgroup, $constr, $format, $ratchet, $csv );
 GetOptions(
     'outgroup=s'   => \$outgroup,
     'constraint=s' => \$constr,
     'format=s'     => \$format,
     'ratchet=s'    => \$ratchet,
+    'csv=s'        => \$csv,
 );
+
+# instantiate map
+my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
 
 # parse constraint tree (typically ncbi tree)
 my ($tree) = @{ parse(
@@ -21,20 +26,40 @@ my ($tree) = @{ parse(
     '-as_project' => 1,
 )->get_items(_TREE_) };
 
-# replace spaces with underscores, remove quotes, unset branch lengths
+# replace names with codes, unset branch lengths
+my %seen;
 $tree->visit(sub{
     my $node = shift;
     if ( $node->is_terminal ) {
         my $name = $node->get_name;
-        $name =~ s/ /_/g;
         $name =~ s/'//g;
-        $node->set_name($name);
+        my $code = $map->get_code_for_binomial($name);
+        $node->set_name($code);
+        $seen{$code} = [] unless $seen{$code};
+        push @{ $seen{$code} }, $node;
     }    
     $node->set_branch_length(undef);
 });
 
 # serialize back to newick
 my $newick = $tree->to_newick;
+
+# prune now duplicated tips
+for my $code ( keys %seen ) {
+    if ( scalar( @{ $seen{$code} } ) > 1 ) {
+        if ( $tree->is_clade( $seen{$code} ) ) {
+            my @codes;
+            for my $i ( 1 .. scalar( @{ $seen{$code} } ) ) {
+                push @codes, $code;
+            }
+            my $pattern = '(' . join(',',@codes) . ')';
+            $newick =~ s/\Q$pattern\E/$code/;
+        }
+        else {
+            warn "problem with $code";
+        }
+    }
+}
 
 # print command blocks
 print <<"FOOTER";
@@ -46,5 +71,6 @@ BEGIN PAUP;
 		OUTGROUP ${outgroup};
 		CONSTRAINTS ncbi (BACKBONE) = ${newick}
 		EXECUTE ${ratchet};
+        QUIT;
 END;
 FOOTER
