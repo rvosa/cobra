9eb41cb44cae91dea4f4423ec9f67d0f11936580
Rutger Vos <rutgeraldo@gmail.com>
2012-04-18 18:42:46 +0200

    updated conversion script

--- script/nexus2newick.pl
+++ script/nexus2newick.pl
@@ -2,15 +2,28 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Bio::Phylo::IO 'parse';
-use Bio::Phylo::Util::CONSTANT ':objecttypes';
 use Bio::Phylo::Cobra::TaxaMap;
+use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::IO qw'parse parse_matrix';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # process command line arguments
-my ( $infile, $csv );
+my ( $infile, $csv, $seqfile );
 GetOptions(
-    'infile=s' => \$infile,
-    'csv=s'    => \$csv,
+    'infile=s'  => \$infile,
+    'seqfile=s' => \$seqfile,
+    'csv=s'     => \$csv,
+);
+
+# instantiate helper objects
+my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
+my $log = Bio::Phylo::Util::Logger->new;
+
+# fetch matrix
+my $matrix = parse_matrix(
+    '-format' => 'fasta',
+    '-file'   => $seqfile,
+    '-type'   => 'dna',    
 );
 
 # fetch first tree
@@ -22,8 +35,24 @@ my ($tree) = @{
     )->get_items(_TREE_)
 };
 
-# instantiate taxa map
-my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
+# prune tips not in alignment
+my @prune_me;
+my %in_matrix;
+$matrix->visit(
+    sub {
+        my $row = shift;
+        my $label = $map->parse_label($row->get_name);
+        $in_matrix{$label} = 1;
+    }
+);
+for my $tip ( @{ $tree->get_terminals } ) {
+    my $label = $map->parse_label($tip->get_name);
+    if ( not $in_matrix{$label} ) {
+        $log->warn("*** Pruning $label");
+        push @prune_me, $tip;
+    }
+}
+$tree->prune_tips(\@prune_me);
 
 # rename tips
 for my $tip ( @{ $tree->get_terminals } ) {
