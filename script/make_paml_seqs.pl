08804f1fcfd97659767995f98a93ed6f8eb6919b
Rutger Vos <rutgeraldo@gmail.com>
2012-06-21 11:02:24 +0200

    adding script to generate PAML alignments

---
+++ script/make_paml_seqs.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Getopt::Long;
+use Bio::Phylo::IO 'parse_matrix';
+
+# process command line arguments
+my $infile;
+GetOptions(
+	'infile=s' => \$infile,
+);
+
+# parse matrix
+my $matrix = parse_matrix(
+	'-format' => 'phylip',
+	'-file'   => $infile,
+	'-type'   => 'dna',
+);
+
+# needs to be multiple of 3
+my $nchar = $matrix->get_nchar;
+my $remainder = $nchar % 3;
+my $pamlnchar = $nchar - $remainder;
+my $ntax = $matrix->get_ntax;
+
+# print phylip header
+print $ntax, ' ', $pamlnchar, "\n";
+
+# print sequences
+for my $row ( @{ $matrix->get_entities } ) {
+	my $name = $row->get_name;
+	my $seq  = $row->get_char;
+	my $codons = substr $seq, 0, $pamlnchar;
+	print $name, '  ', $codons, "\n";
+}
