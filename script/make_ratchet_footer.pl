7d6dfa4b533f4343d5e09a1e20109eead7cbe31a
Rutger Vos <rutgeraldo@gmail.com>
2012-08-09 17:01:48 +0400

    Updated to take into account that we now use the NCBI common tree where, if there are subspecies, the species becomes an internal node. We turn this internal node into a polytomous sibling of the subspecies.

--- script/make_ratchet_footer.pl
+++ script/make_ratchet_footer.pl
@@ -2,22 +2,27 @@
 use warnings;
 use strict;
 use Getopt::Long;
+use Bio::Phylo::Factory;
 use Bio::Phylo::IO 'parse';
 use Bio::Phylo::Cobra::TaxaMap;
+use Bio::Phylo::Util::Logger;
 use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # process command line arguments
-my ( $outgroup, $constr, $format, $ratchet, $csv );
+my ( $outgroup, $constr, $format, $ratchet, $csv, $verbosity );
 GetOptions(
     'outgroup=s'   => \$outgroup,
     'constraint=s' => \$constr,
     'format=s'     => \$format,
     'ratchet=s'    => \$ratchet,
     'csv=s'        => \$csv,
+    'verbose+'     => \$verbosity,
 );
 
-# instantiate map
+# instantiate helper objects
 my $map = Bio::Phylo::Cobra::TaxaMap->new($csv);
+my $log = Bio::Phylo::Util::Logger->new( '-class' => 'main', '-level' => $verbosity );
+my $fac = Bio::Phylo::Factory->new;
 
 # parse constraint tree (typically ncbi tree)
 my ($tree) = @{ parse(
@@ -26,40 +31,43 @@ my ($tree) = @{ parse(
     '-as_project' => 1,
 )->get_items(_TREE_) };
 
-# replace names with codes, unset branch lengths
-my %seen;
-$tree->visit(sub{
-    my $node = shift;
-    if ( $node->is_terminal ) {
-        my $name = $node->get_name;
-        $name =~ s/'//g;
-        my $code = $map->get_code_for_binomial($name);
-        $node->set_name($code);
-        $seen{$code} = [] unless $seen{$code};
-        push @{ $seen{$code} }, $node;
-    }    
-    $node->set_branch_length(undef);
-});
+# here we map all binomials into species codes
+my @binomials = $map->get_distinct_binomials;
+for my $binomial ( @binomials ) {
+	my $node;
+	my $code = $map->get_code_for_binomial($binomial);
+	
+	# matches the entire name
+	my $regex_full = qr/^\Q'$binomial'\E$/;
+	
+	# matches first parts (so could match subspecies)
+	my $regex_partial = qr/^\Q'$binomial\E/;
+	
+	# these are just to shorten the subsequent invocation
+	my %param  = ( '-value' => 'get_name' );
+	my $method = 'get_by_regular_expression';
+	
+	# we have an exact match, simply rename to code
+	if ( $node = $tree->$method( %param, '-match' => $regex_full )->[0] ) {
+		$node->set_name( $code );
+	}
+	
+	# we match a subspecies, which we need to give a sibling
+	elsif ( $node = $tree->$method( %param, '-match' => $regex_partial )->[0] ) {
+		my $sibling = $fac->create_node( '-name' => $code );
+		$node->get_parent->set_child($sibling);
+		$tree->insert($sibling);
+	}
+	else {
+		$log->warn("NCBI taxonomy has no node for $binomial");
+	}
+}
+
+# remove those :4 branch lengths that NCBI adds for some reason
+$tree->visit(sub{shift->set_branch_length(undef)});
 
 # serialize back to newick
 my $newick = $tree->to_newick;
-
-# prune now duplicated tips
-for my $code ( keys %seen ) {
-    if ( scalar( @{ $seen{$code} } ) > 1 ) {
-        if ( $tree->is_clade( $seen{$code} ) ) {
-            my @codes;
-            for my $i ( 1 .. scalar( @{ $seen{$code} } ) ) {
-                push @codes, $code;
-            }
-            my $pattern = '(' . join(',',@codes) . ')';
-            $newick =~ s/\Q$pattern\E/$code/;
-        }
-        else {
-            warn "problem with $code";
-        }
-    }
-}
 
 # print command blocks
 print <<"FOOTER";
