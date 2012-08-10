ccf0bc8e77c554a7086471760d2a8d99fe864d36
Rutger Vos <rutgeraldo@gmail.com>
2012-08-10 11:18:45 +0400

    added logging to warn of possible truncation

--- script/make_paml_seqs.pl
+++ script/make_paml_seqs.pl
@@ -2,14 +2,19 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Bio::Phylo::Util::Logger;
 use Bio::Phylo::IO qw'parse_matrix parse_tree';
 
 # process command line arguments
-my ( $infile, $treefile );
+my ( $infile, $treefile, $verbose );
 GetOptions(
 	'infile=s'   => \$infile,
 	'treefile=s' => \$treefile,
+	'verbose+'   => \$verbose,
 );
+
+# instantiate logger
+my $log = Bio::Phylo::Util::Logger->new( '-class' => 'main', '-level' => $verbose );
 
 # parse matrix
 my $matrix = parse_matrix(
@@ -31,6 +36,7 @@ $matrix->visit(sub{
 	my $row = shift;
 	my $name = $row->get_name;
 	if ( not $tips{$name} ) {
+		$log->info("going to remove $name from $infile");
 		push @delete, $row;	
 	}
 });
@@ -41,6 +47,7 @@ my $nchar = $matrix->get_nchar;
 my $remainder = $nchar % 3;
 my $pamlnchar = $nchar - $remainder;
 my $ntax = $matrix->get_ntax;
+$log->warn("going to truncate $infile by $remainder characters") if $remainder;
 
 # print phylip header
 print '  ', $ntax, '  ', $pamlnchar, "\n";
